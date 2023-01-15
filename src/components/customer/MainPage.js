8f2c0072b9aa298ca2ab5e02ae78399a5f171055
Rajab <rkarume@gmail.com>
2023-01-15 23:06:34 +0300

    Created and styled sidebar

--- src/components/customer/MainPage.js
+++ src/components/customer/MainPage.js
@@ -5,8 +5,60 @@ import "./Customer.css";
 function MainPage(){
 
     return(
-        <div>
-            <h1>Customer</h1>
+        <div className="mainpage">
+            <div className="header">
+                <h1>Customer</h1>
+            </div>
+            <div className="table">
+                <table>
+                    <thead>
+                        <tr>
+                        <th>Handyman details</th>
+                        <th>Handyman name</th>
+                        <th>Date</th>
+                        <th>Speciality</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>john@email.com</td>
+                            <td>john doe</td>
+                            <td>nov 10, 1997</td>
+                            <td>Plumber</td>
+                        </tr>
+                        <tr>
+                            <td>john@email.com</td>
+                            <td>john doe</td>
+                            <td>nov 10, 1997</td>
+                            <td>Plumber</td>
+                        </tr>
+                        <tr>
+                            <td>john@email.com</td>
+                            <td>john doe</td>
+                            <td>nov 10, 1997</td>
+                            <td>Plumber</td>
+                        </tr>
+                        <tr>
+                            <td>john@email.com</td>
+                            <td>john doe</td>
+                            <td>nov 10, 1997</td>
+                            <td>Plumber</td>
+                        </tr>
+                        <tr>
+                            <td>john@email.com</td>
+                            <td>john doe</td>
+                            <td>nov 10, 1997</td>
+                            <td>Plumber</td>
+                        </tr>
+                        <tr>
+                            <td>john@email.com</td>
+                            <td>john doe</td>
+                            <td>nov 10, 1997</td>
+                            <td>Plumber</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
--- src/components/customer/Sidebar.js
+++ src/components/customer/Sidebar.js
@@ -8,21 +8,48 @@ function Sidebar(){
     return(
         <div className="sidebar">
             <div className="sidebar-header">
-                <h1>Admin Dashboard</h1>
+                <h1 className="sidebar-h1">Admin Dashboard</h1>
             </div>
             <div className="sidebar-options">
-                <ul>
-                    <li>Overview</li>
-                    <li>Handymen</li>
-                    <li>Jobs</li>
-                    <li>Customers</li>
-                </ul>
+                <div className="options">
+                    <img alt="" className="options-icon" src="https://cdn-icons-png.flaticon.com/128/3524/3524636.png" />
+                    <div className="option-info">
+                        <span className="option-name">Overview</span>
+                    </div>
+                </div>
+                <div className="options">
+                    <img alt="" className="options-icon" src="https://cdn-icons-png.flaticon.com/128/3524/3524636.png" />
+                    <div className="option-info">
+                        <span className="option-name">Handymen</span>
+                    </div>
+                </div>
+                <div className="options">
+                    <img alt="" className="options-icon" src="https://cdn-icons-png.flaticon.com/128/3524/3524636.png" />
+                    <div className="option-info">
+                        <span className="option-name">Jobs</span>
+                    </div>
+                </div>
+                <div className="options">
+                    <img alt="" className="options-icon" src="https://cdn-icons-png.flaticon.com/128/3524/3524636.png" />
+                    <div className="option-info">
+                        <span className="option-name">Customers</span>
+                    </div>
+                </div>
             </div>
             <div className="sidebar-bottom">
-                <ul>
-                    <li>Settings</li>
-                    <li>Log Out</li>
-                </ul>
+                <div className="options">
+                    <img alt="" className="options-icon" src="https://cdn-icons-png.flaticon.com/128/3524/3524636.png" />
+                    <div className="option-info">
+                        <span className="option-name">Settings</span>
+                    </div>
+                </div>
+                <div className="options">
+                    <img alt="" className="options-icon" src="https://cdn-icons-png.flaticon.com/128/3524/3524636.png" />
+                    <div className="option-info">
+                        <span className="option-name">Log out</span>
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
