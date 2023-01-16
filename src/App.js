9fbdff09c4da3d37c96e17ec50ac4301fcbe9e93
Rlynette <lynette.ronnoh@student.moringaschool.com>
2023-01-16 13:48:05 +0300

    import react-icons

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Customer from './components/customer/Customer';
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Handyman from './components/handyman/Handyman';
 import Login from './components/Login/Login';
-
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/customer" element={<Customer />} />
           <Route path="/handyman" element={<Handyman />} />
-          <Route path='/' element={<Login/>} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+
 
 function Dashboard() {
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <>
       <div className='dashboard'>
       <Link to="#" className='menu-bars'>
-
+     {/* <GrIcons.GrOverview/> */}
+    <FaIcons.FaBars/>
       </Link>
       </div>
-    </>
-  )
+       <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className='nav-menu-items'>
+            <li className='navbar-toggle'>
+                <Link to="#" className='menu-bars'>
+                    <AiIcons.AiOutlineCloseCircle/>
+                </Link>
+            </li>
+        </ul>
+       </nav>
+      </>
+  );
 }
 
 export default Dashboard
