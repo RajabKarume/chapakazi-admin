a24d72f87c479f97cbab0d3b87095fed874a8d77
Rlynette <lynette.ronnoh@student.moringaschool.com>
2023-01-18 22:41:02 +0300

    edit components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Handyman from './components/handyman/Handyman';
 import Login from './components/Login/Login';
 import Dashboard from './components/Dashboard/Dashboard';
-import Overview from "./components/Overview/Overview";
+// import Overview from "./components/Overview/Overview";
 import Signup from "./components/SignUp/Signup"
 
 
@@ -16,7 +16,7 @@ function App() {
           <Route path="/dashboard/" element={<Dashboard />} />
           <Route path="/customer" element={<Customer />} />
           <Route path="/handyman" element={<Handyman />} />
-          <Route path="/overview/" element={<Overview />} />
+          {/* <Route path="/overview/" element={<Overview />} /> */}
         {/* <Route path="/settings" element={<Settings />} /> */}
           <Route path="/adminSignup" element={<Signup />} />
           <Route path="/" element={<Login />} />
