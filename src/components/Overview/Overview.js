ade07a5afab078e66fa24fbefc34e9b067e14f23
Rlynette <lynette.ronnoh@student.moringaschool.com>
2023-01-18 16:10:04 +0300

    slight change

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Dashboard from "../Dashboard/Dashboard";
-
+import * as FaIcons from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import "./Overview.css"
 
 
 function Overview(){
+
+  const [trigger, setTrigger] = useState(false);
     
 
 //     const [handyman, setHandyman] = useState([])
@@ -18,9 +23,41 @@ function Overview(){
 //     },[token]) 
 //     console.log(handyman)
 
-    return(
-        <div>
-{/* //         <div className="customer">
+    return (
+      <div className="main-header">
+        {/* <nav className="main-header"> */}
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              Overview
+            </a>
+          </li>
+
+            <ul>
+              {/* {" "} */}
+              <li className="icons">
+                <FaIcons.FaSearch />
+              </li>
+              <li>
+                <FaIcons.FaBell />
+              </li>
+              <li>
+                {/* <a href="#">Jones Ferdinand</a> */}
+                <h4>Jones Ferdinand</h4>
+                <FontAwesomeIcon
+                  icon={faUser}
+                  className="user-profile"
+                  onClick={() => {
+                    setTrigger((prev) => !prev);
+                  }}
+                />
+              </li>
+            </ul>
+        
+        </ul>
+
+        {/* </nav> */}
+        {/* //         <div className="customer">
 //             <div className="customer-side">
 //                 <Dashboard />
 //             </div>
@@ -28,8 +65,8 @@ function Overview(){
 //                 <HandymanMain header="Handyman" handyman={handyman} />
 //             </div>
 //         </div> */}
-        </div>
-    )
+      </div>
+    );
 }
 
 export default Overview
